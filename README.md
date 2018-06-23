877904d591c6e0c3b9ff584b3729e33911a83df3
adekcz <adekcz@gmail.com>
2018-06-23 23:12:19 +0200

    Spring boot project skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+# Juggly - juggling records
+
+Site that allows you to track, compare and share your juggling accomplishments.
+
+
+
+
+## Authors
+coding by: Michal Keda
+design by: Vasek Peca
---
+++ src/main/java/ly/jugg/juggly/JugglyApplication.java
@@ -0,0 +1,12 @@
+package ly.jugg.juggly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugglyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugglyApplication.class, args);
+	}
+}
